3ea1216fbe6919853873130c1f39652bd46dd758
Adam Gask <ajgask@gmail.com>
2016-10-24 16:33:45 +0100

    Added Swift 101 - var vs let playground

---
+++ Swift 101 - let vs var.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//let variables
+let numberOfMonths = 12
+//If you uncomment the line below, the Swift compiler will show you an error.
+//numberOfMonths = 11
+
+//var variables
+var roomTemperature = 21.5
+//It gets warmer!
+roomTemperature = 23
+
+//Your favourite coffee shop example
+
+//Start of the day
+let numberOfChairs = 25
+var numberOfCustomers = 0
+//Open the shop doors and 5 customers arrive
+numberOfCustomers = numberOfCustomers + 5 // The number of customers is now 5. Therefore the numberOfCustomers must be a mutable variable.
